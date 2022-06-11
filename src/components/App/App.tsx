829fd333abe443caed176e12b1497de454708ab9
yhrage524 <yhrage524@gmail.com>
2022-06-12 01:12:57 +0300

    add HeaderContainer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import s from './App.module.sass'
 import Header from '../Header/Header'
+import HeaderContainer from '../HeaderContainer/HeaderContainer'
 
 const App = () => {
     return (
         <div className={ s.App }>
             <Header/>
+            <HeaderContainer/>
         </div>
     )
 }
---
+++ src/components/HeaderContainer/HeaderContainer.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import s from './HeaderContainer.module.sass'
+
+const HeaderContainer: React.FC = () => {
+    return (
+        <div
+            className={ s.wrapper }
+        >
+            <span className={ s.block_name }>
+                Food app
+            </span>
+            <h1 className={ s.title }>
+                Why stay hungry when<br/>you can order form Bella Onojie
+            </h1>
+            <h4 className={ s.sub_title }>
+                Download the bella onoje’s food app now on
+            </h4>
+            <div className={ s.btn_wrapper }>
+                <button className={ s.btn + ' ' + s.red_btn }>Playstore</button>
+                <button className={ s.btn + ' ' + s.white_btn }>App store</button>
+            </div>
+        </div>
+    )
+}
+
+export default HeaderContainer
